3b158817dff0aa6fed201758ffa222d171a6f27c
haysch <36488481+haysch@users.noreply.github.com>
2019-01-29 20:16:02 +0100

    Refactor code. Change method accessibility.

--- Image.cs
+++ Image.cs
@@ -7,10 +7,10 @@ namespace PhotoOrganizer {
     class Image {
         public string DateTimeOriginal { get; internal set; }
         public ushort ISO { get; internal set; }   
-        public Rational Aperture { get; internal set; }
-        // TODO Fnumber
-        public Rational ShutterSpeed { get; internal set; }
-        public Rational FocalLength { get; internal set; }
+        public double Aperture { get; internal set; }
+        public string FNumber { get; internal set; }
+        public double ShutterSpeed { get; internal set; }
+        public double FocalLength { get; internal set; }
         public string Make { get; internal set; }
         public string Model { get; internal set; }
         public int Width { get; internal set; }
@@ -19,12 +19,16 @@ namespace PhotoOrganizer {
         public double Latitude { get; internal set; }
         public double Altitude { get; internal set; }
 
-        private Bitmap img;
+        public int HashValue { get; internal set; }
+
+        private Bitmap image;
+        private string filePath;
 
         // TODO add hashmap when initializing to get all propItems and call when needed ?
         public Image(string imgName, string path) {
             // TODO test deeper enumeration of path (subfolders)
-            img = new Bitmap(path + Path.DirectorySeparatorChar + imgName);
+            filePath = path + Path.DirectorySeparatorChar + imgName;
+            image = new Bitmap(filePath);
             
             // var propItems = img.PropertyItems;
 
@@ -35,10 +39,11 @@ namespace PhotoOrganizer {
 
         /// <summary> Extracts the DateTimeOriginal property from image and converts it to ASCII string. </summary>
         /// <remarks> If the prop does not exist, assign variable to default value. </remarks>
-        public void extractDateTimeOriginal() {
+        private void ExtractDateTimeOriginal() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.DateTime);
-                DateTimeOriginal = (string)PropertyTag.getValue(propItem);
+                var propItem = image.GetPropertyItem((int)TagConstants.DTOrig);
+                var dtOrig = (string)PropertyTag.getValue(propItem);
+                DateTimeOriginal = ExifDTToDateTime(dtOrig);
             } catch (Exception) {
                 // if there is no value set to default
                 DateTimeOriginal = "";
@@ -47,9 +52,9 @@ namespace PhotoOrganizer {
 
         /// <summary> Extracts the ISO property from image and converts it to short string. </summary>
         /// <remarks> If the prop does not exist, assign variable to default value. </remarks>
-        public void extractISO() {
+        private void ExtractISO() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.ISO);
+                var propItem = image.GetPropertyItem((int)TagConstants.ISO);
                 ISO = (ushort)PropertyTag.getValue(propItem);
             } catch (Exception) {
                 // if there is no value set to default
@@ -57,39 +62,48 @@ namespace PhotoOrganizer {
             }
         }
 
-        public void extractAperture() {
+        private void ExtractAperture() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.Aperture);
+                var propItem = image.GetPropertyItem((int)TagConstants.Aperture);
                 Aperture = (Rational)PropertyTag.getValue(propItem);
             } catch (Exception) {
                 // if there is no value set to default
-                Aperture = new Rational(0);
+                Aperture = 0.0;
             }
         }
 
-        public void extractShutterSpeed() {
+        private void ExtractFNumber() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.ShutterSpeed);
+                var propFNum = image.GetPropertyItem((int)TagConstants.FNumber);
+                FNumber = ((Rational)PropertyTag.getValue(propFNum)).ToFNumber();
+            } catch (Exception) {
+                FNumber = "";
+            }
+        }
+
+        private void ExtractShutterSpeed() {
+            try {
+                var propItem = image.GetPropertyItem((int)TagConstants.ShutterSpeed);
                 ShutterSpeed = (Rational)PropertyTag.getValue(propItem);
             } catch (Exception) {
                 // if there is no value set to default
-                ShutterSpeed = new Rational(0);
+                ShutterSpeed = 0.0;
             }
         }
 
-        public void extractFocalLength() {
+        private void ExtractFocalLength() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.FocalLength);
+                var propItem = image.GetPropertyItem((int)TagConstants.FocalLength);
                 FocalLength = (Rational)PropertyTag.getValue(propItem);
             } catch (Exception) {
                 // if there is no value set to default
-                FocalLength = new Rational(0);
+                FocalLength = 0.0;
             }
         }
 
-        public void extractMake() {
+        private void ExtractMake() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.Make);
+                var propItem = image.GetPropertyItem((int)TagConstants.Make);
                 Make = (string)PropertyTag.getValue(propItem);
             } catch (Exception) {
                 // if there is no value set to default
@@ -97,9 +111,9 @@ namespace PhotoOrganizer {
             }
         }
 
-        public void extractModel() {
+        private void ExtractModel() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.Model);
+                var propItem = image.GetPropertyItem((int)TagConstants.Model);
                 Model = (string)PropertyTag.getValue(propItem);
             } catch (Exception) {
                 // if there is no value set to default
@@ -107,45 +121,16 @@ namespace PhotoOrganizer {
             }
         }
 
-        // TODO does not get resolution - something wrong with width/height or multiplier
-        public void GetResolution() {
-            try {
-                var propWidth = img.GetPropertyItem((int)TagConstants.XResolution);
-                var resWidth = (Rational)PropertyTag.getValue(propWidth);
-                var propHeight = img.GetPropertyItem((int)TagConstants.YResolution);
-                var resHeight = (Rational)PropertyTag.getValue(propHeight);
-
-                Console.WriteLine("Width: {0}\nHeight: {1}", resWidth, resHeight);
-
-            } catch (Exception) {
-
-            }
+        private void ExtractResolution() {
+            Width = image.Width;
+            Height = image.Height;
         }
-        public void extractWidth() {
-            try {
-                var propItem = img.GetPropertyItem((int)TagConstants.XResolution);
-                Width = (int)PropertyTag.getValue(propItem);
-            } catch (Exception) {
-                // if there is no value set to default
-                Width = 0;
-            }
-        }
-
-        public void extractHeight() {
-            try {
-                var propItem = img.GetPropertyItem((int)TagConstants.YResolution);
-                Height = (int)PropertyTag.getValue(propItem);
-            } catch (Exception) {
-                // if there is no value set to default
-                Height = 0;
-            }
-        }
-
+        
         /// <summary> Gets the latitude from the image in decimal format </summary>
-        public void extractLatitude() {
+        private void ExtractLatitude() {
             try {
-                var propGpsLat = img.GetPropertyItem((int)TagConstants.Latitude);
-                var propGpsLatRef = img.GetPropertyItem((int)TagConstants.GpsLatitudeRef);
+                var propGpsLat = image.GetPropertyItem((int)TagConstants.Latitude);
+                var propGpsLatRef = image.GetPropertyItem((int)TagConstants.GpsLatitudeRef);
 
                 Rational[] latitudeDegMinSecs = (Rational[])PropertyTag.getValue(propGpsLat);
                 string GpsLatRef = (string)PropertyTag.getValue(propGpsLatRef);
@@ -157,10 +142,10 @@ namespace PhotoOrganizer {
             }
         }
 
-        public void extractLongitude() {
+        private void ExtractLongitude() {
             try {
-                var propGpsLong = img.GetPropertyItem((int)TagConstants.Longitude);
-                var propGpsLongRef = img.GetPropertyItem((int)TagConstants.GpsLongitudeRef);
+                var propGpsLong = image.GetPropertyItem((int)TagConstants.Longitude);
+                var propGpsLongRef = image.GetPropertyItem((int)TagConstants.GpsLongitudeRef);
 
                 Rational[] longitudeDegMinSecs = (Rational[])PropertyTag.getValue(propGpsLong);
                 string GpsLongRef = (string)PropertyTag.getValue(propGpsLongRef);
@@ -177,21 +162,58 @@ namespace PhotoOrganizer {
 
             double value = degMinSec[0] + (degMinSec[1] / 60d) + (degMinSec[2] / 3600d);
             
+            // If Ref is not N or E, negate the value.
             if (gpsRef == "S" || gpsRef == "W") value *= -1;
             return value;
         }
 
-        public void extractAltitude() {
-            string description;
-            
+        private void ExtractAltitude() {
             try {
-                var propItem = img.GetPropertyItem((int)TagConstants.Altitude);
-                Altitude = (Rational)PropertyTag.getValue(propItem);
-            } catch (Exception e) {
+                var propertyAltitude = image.GetPropertyItem((int)TagConstants.Altitude);
+                var propertyAltitudeRef = image.GetPropertyItem((int)TagConstants.GpsAltitudeRef);
+                var altRef = (int)PropertyTag.getValue(propertyAltitudeRef);
+
+                Console.WriteLine(altRef);
+
+                Altitude = (Rational)PropertyTag.getValue(propertyAltitude);
+            } catch (Exception) {
                 // if there is no value set to default
-                description = $"EXCEPTION: {e.Message}";
                 Altitude = 0;
             }
+        }
+
+        public void ExtractMetadata() {
+            ExtractDateTimeOriginal();
+            ExtractISO();
+            ExtractAperture();
+            ExtractFNumber();
+            ExtractShutterSpeed();
+            ExtractFocalLength();
+            ExtractMake();
+            ExtractModel();
+            ExtractResolution();
+            ExtractLatitude();
+            ExtractLongitude();
+            ExtractAltitude();
+        }
+
+        public static int MD5HashFunction() {
+            return 0; // TODO make function for getting MD5 hash of image
+        }
+
+        /// <summary> Converts the Exif DateTime format to yyyyMMdd_hhMMss </summary>
+        private static string ExifDTToDateTime(string dtOrig) {
+            dtOrig = dtOrig.Replace(' ', ':');
+            string[] ymdHms = dtOrig.Split(':');
+
+            string year    = ymdHms[0];
+            string month   = ymdHms[1];
+            string day     = ymdHms[2];
+            string hour    = ymdHms[3];
+            string minute  = ymdHms[4];
+            string second  = ymdHms[5];
+            
+            return year + month + day + "_" + hour + minute + second;
         }
     }
 }
