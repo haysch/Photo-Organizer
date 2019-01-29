d76c2acf84b27718ab696ebe945a2a1d40bae415
haysch <36488481+haysch@users.noreply.github.com>
2019-01-29 20:16:32 +0100

    Restructure of main flow. Now prints info.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace PhotoOrganizer
 {
@@ -7,41 +8,43 @@ namespace PhotoOrganizer
     {
         static void Main(string[] args)
         {
-            // Image test = new Image("fakeImg.jpg", "testdata");
-            Image test2 = new Image("img1.jpg", "testdata");
-            Image test3 = new Image("Food.jpg", "testdata");
-            Image test4 = new Image("img2.jpg", "testdata");
+            int i = 0;
+            List<Image> imgList = new List<Image>();
+            imgList.Add(new Image("fakeImg.jpg", "testdata"));
+            imgList.Add(new Image("img1.jpg", "testdata"));
+            imgList.Add(new Image("Food.jpg", "testdata"));
+            imgList.Add(new Image("img2.jpg", "testdata"));
 
-            // test.extractDateTime();
-            // test2.extractDateTime();
-            // test4.extractDateTime();
-            // test3.extractDateTime();
+            foreach (Image img in imgList) {
+                img.ExtractMetadata();
+                i++;
 
-            test2.extractLatitude();
-            test2.extractLongitude();
+                PrintMetadata(img, i, imgList.Count);
+            }
+        }
 
-            Console.WriteLine("Lat: {0}\tLong: {1}", test2.Latitude, test2.Longitude);
+        private List<Image> LoadImages() {
+            List<Image> imageList = new List<Image>();
 
-            test3.extractLatitude();
-            test3.extractLongitude();
+            // TODO enumerate folders at i depth and 
 
-            Console.WriteLine("Lat: {0}\tLong: {1}", test3.Latitude, test3.Longitude);
+            return imageList;
+        }
 
-            test4.extractLatitude();
-            test4.extractLongitude();
-
-            Console.WriteLine("Lat: {0}\tLong: {1}", test4.Latitude, test4.Longitude);
-            
-            // test4.extractModel();
-            
-            // test4.GetResolution();
-
-            // Console.WriteLine("Make: {0}\nModel: {1}\nWidth: {2}\nHeight: {3}", test4.Make, test4.Model, test4.Width, test4.Height);
-
-            //Console.WriteLine(test4.Longitude);
-            // Console.WriteLine(test3.DateTimeOriginal);
-            // Console.WriteLine(test4.Aperture);
-            // Console.WriteLine(test2.DateTimeOriginal);
+        public static void PrintMetadata(Image image, int current, int max) {
+            Console.WriteLine("================={0}/{1}=================", current, max);
+            Console.WriteLine("DateTime Original: {0}", image.DateTimeOriginal);
+            Console.WriteLine("ISO: {0}", image.ISO);
+            Console.WriteLine("Aperture: {0}", image.Aperture);
+            Console.WriteLine("F-Number: {0}", image.FNumber);
+            Console.WriteLine("Shutter Speed: {0}", image.ShutterSpeed);
+            Console.WriteLine("Focal Length: {0}", image.FocalLength);
+            Console.WriteLine("Make: {0}\tModel: {1}", image.Make, image.Model);
+            Console.WriteLine("Width: {0}\tHeight: {1}", image.Width, image.Height);
+            Console.WriteLine("Lat/Long: {0:N6}, {1:N6}", image.Latitude, image.Longitude);
+            Console.WriteLine("Altitude (meters): {0}", image.Altitude);
+            Console.WriteLine("Hash value (MD5): {0}", image.HashValue);
+            Console.WriteLine("=====================================\n");
         }
     }
 }
