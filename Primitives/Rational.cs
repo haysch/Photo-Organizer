ef96cc4af247fd46ed0e7a86fa8c9cac3601dce6
haysch <36488481+haysch@users.noreply.github.com>
2019-01-31 20:10:18 +0100

    Move helper classes to own namespace. Remove some constants.

--- Primitives/Rational.cs
+++ Primitives/Rational.cs
@@ -3,16 +3,22 @@ using System;
 namespace PhotoOrganizer.Primitives {
     /// <summary> TODO </summary>
     public  class Rational {
-        long Numerator { get; set; }
-        long Denominator { get; set; }
+        public long Numerator { get; }
+        public long Denominator { get; }
 
         public Rational(long num) {
             Numerator = num;
+            Denominator = 1;
         }
 
         public Rational(long num, long den) {
             Numerator = num;
             Denominator = den;
+        }
+
+        public Rational(uint num, uint den) {
+            Numerator = Convert.ToInt64(num);
+            Denominator = Convert.ToInt64(den);
         }
 
         /// <summary> Returns the Rational as a fraction of form 15/25 </summary>
@@ -22,7 +28,11 @@ namespace PhotoOrganizer.Primitives {
         }
 
         /// <summary> Returns the Rational on the f-number form. </summary>
-        public string ToFNumber() => $"f/{(Numerator / Denominator)}";
+        public string ToFNumber() {
+            if ((Numerator / Denominator) % 1 == 0)
+                return $"f/{(Numerator / Denominator)}";
+            return $"f/{(Numerator / Denominator):N1}";
+        }
 
         /// <summary> Implicit casts Rational to double. </summary>
         public static implicit operator double(Rational r) => r.Numerator == 0 ? 0.0 : (double)r.Numerator / r.Denominator;
