c71376e56722eb8912d4d616054c1e1d63e291dd
haysch <36488481+haysch@users.noreply.github.com>
2019-01-31 14:09:41 +0100

    Move to own namespace

--- Primitives/Rational.cs
+++ Primitives/Rational.cs
@@ -1,28 +1,28 @@
 using System;
 
-namespace PhotoOrganizer {
+namespace PhotoOrganizer.Primitives {
     /// <summary> TODO </summary>
     public  class Rational {
-        long Numerator { get; }
-        long Denominator { get; }
+        long Numerator { get; set; }
+        long Denominator { get; set; }
 
         public Rational(long num) {
             Numerator = num;
         }
 
-        public Rational (long num, long den) {
+        public Rational(long num, long den) {
             Numerator = num;
             Denominator = den;
         }
 
+        /// <summary> Returns the Rational as a fraction of form 15/25 </summary>
         public override string ToString() {
-            if (this.Denominator == 1) return $"{this.Numerator}";
+            if (this.Denominator == 1) return $"{Numerator}";
             return $"{Numerator}/{Denominator}";
         }
 
-        public string ToFNumber() {
-            return $"f/{(Numerator / Denominator)}";
-        }
+        /// <summary> Returns the Rational on the f-number form. </summary>
+        public string ToFNumber() => $"f/{(Numerator / Denominator)}";
 
         /// <summary> Implicit casts Rational to double. </summary>
         public static implicit operator double(Rational r) => r.Numerator == 0 ? 0.0 : (double)r.Numerator / r.Denominator;
