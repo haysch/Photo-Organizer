da6004b04e149f126d301bf663ab68cc5977e041
haysch <36488481+haysch@users.noreply.github.com>
2019-02-12 22:18:38 +0100

    Refactor code to enforce curly brace C# convention

--- Util/Checksum.cs
+++ Util/Checksum.cs
@@ -3,35 +3,40 @@ using System.IO;
 using System.Text;
 using System.Security.Cryptography;
 
-namespace PhotoOrganizer.Util {
+namespace PhotoOrganizer.Util
+{
     /// <summary>Available hash algorithms</summary>
-    public enum Algorithm : int {
+    public enum Algorithm : int
+    {
         /// <summary>Message-Digest Algorithm 5</summary>
         /// <remarks>128 bit digest size</remarks>
-        MD5 = 0,
+        MD5,
         /// <summary>Secure Hash Algorithm 1</summary>
         /// <remarks>160 bit digest size</remarks>
-        SHA1 = 1,
+        SHA1,
         /// <summary>Secure Hash Algorithm 2</summary>
         /// <remarks>256 bit digest size</remarks>
-        SHA256 = 2,
+        SHA256,
         /// <summary>Secure Hash Algorithm 2</summary>
         /// <remarks>384 bit digest size</remarks>
-        SHA384 = 3,
+        SHA384,
         /// <summary>Secure Hash Algorithm 2</summary>
         /// <remarks>512 bit digest size</remarks>
-        SHA512 = 4
+        SHA512
     };
 
     /// <summary>Specifies the Checksum class for use when computing hash values of files</summary>
-    public class Checksum {
+    public class Checksum
+    {
         private HashAlgorithm _cryptoAlgorithm;
         /// <summary>Constructor for the algorithm used for computing file hash.</summary>
         /// <exception>Throws an ArgumentException if input algorithm is not available.</exception>
         /// <param name="algorithm">Algorithm to be used.</param>
         /// See <see cref="Util.Algorithm"/> for available hash algorithms.
-        public Checksum(Algorithm algorithm) {
-            switch(algorithm) {
+        public Checksum(Algorithm algorithm)
+        {
+            switch (algorithm)
+            {
                 case Algorithm.MD5:
                     _cryptoAlgorithm = new MD5CryptoServiceProvider();
                     break;
@@ -55,14 +60,17 @@ namespace PhotoOrganizer.Util {
         /// <summary>Computes the hash value of the file.</summary>
         /// <returns>Hash value of file.</returns>
         /// <param name="path">String path.</param>
-        public string ComputeHash(string path) {
+        public string ComputeHash(string path)
+        {
             if (_cryptoAlgorithm == null || !File.Exists(path)) return "";
 
-            using (FileStream stream = File.OpenRead(path)) {
+            using (FileStream stream = File.OpenRead(path))
+            {
                 byte[] hashBytes = _cryptoAlgorithm.ComputeHash(stream);
 
                 StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < hashBytes.Length; i++) {
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
                     sb.Append(hashBytes[i].ToString("x2"));
                 }
 
