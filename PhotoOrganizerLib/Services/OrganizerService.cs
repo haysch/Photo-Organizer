4199f5241467595c9746a8d119dde0a14486e365
haysch <36488481+haysch@users.noreply.github.com>
2020-06-17 07:41:56 +0200

    Add service for exposing functionality.

---
+++ PhotoOrganizerLib/Services/OrganizerService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using PhotoOrganizerLib.Enums;
+using PhotoOrganizerLib.Interfaces;
+
+namespace PhotoOrganizerLib.Services
+{
+    public class OrganizerService : IOrganizerService
+    {
+        public OrganizerService(ISort sortService)
+        {
+
+        }
+
+        public void RunOrganizer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
