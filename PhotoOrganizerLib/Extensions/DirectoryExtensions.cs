18a3f44f12f7a618cf3785d875e7b387d143c58b
haysch <36488481+haysch@users.noreply.github.com>
2020-06-17 07:35:09 +0200

    Extend ParseMetadata and introduce extension methods. Add metadata converter.

---
+++ PhotoOrganizerLib/Extensions/DirectoryExtensions.cs
@@ -0,0 +1,86 @@
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Jpeg;
+using MetadataExtractor.Formats.Png;
+using PhotoOrganizerLib.Models;
+using PhotoOrganizerLib.Utils;
+
+namespace PhotoOrganizerLib.Extensions
+{
+    public static class DirectoryExtensions
+    {
+        public static void ParseGps(this GpsDirectory directory, Photo photo)
+        {
+            var gpsLatRef = directory.GetString(GpsDirectory.TagLatitudeRef);
+            var gpsLat = directory.GetRationalArray(GpsDirectory.TagLatitude);
+
+            var gpsLonRef = directory.GetString(GpsDirectory.TagLongitudeRef);
+            var gpsLon = directory.GetRationalArray(GpsDirectory.TagLongitude);
+
+            var latitude = MetadataConversion.DegreesMinutesSecondsToDecimal(gpsLat, gpsLatRef);
+            var longitude = MetadataConversion.DegreesMinutesSecondsToDecimal(gpsLon, gpsLonRef);
+
+            photo.AddMetadata("Latitude", latitude);
+            photo.AddMetadata("Longitude", longitude);
+            
+            if (directory.TryGetByte(GpsDirectory.TagAltitudeRef, out var gpsAltBit) &&
+                directory.TryGetInt16(GpsDirectory.TagAltitude, out var gpsAlt))
+            {
+                var gpsAltRef = gpsAltBit == 0 ? "Sea level" : "Below sea level";
+                photo.AddMetadata("AltitudeRef", gpsAltRef);
+                photo.AddMetadata("Altitude", gpsAlt);
+            }
+        }
+
+        public static void ParseIFD0(this ExifIfd0Directory directory, Photo photo)
+        {
+            var make = directory.GetString(ExifIfd0Directory.TagMake);
+            var model = directory.GetString(ExifIfd0Directory.TagModel);
+
+            if (make != null)
+                photo.AddMetadata("Make", make);
+
+            if (model != null)
+                photo.AddMetadata("Model", model);
+
+            if (directory.TryGetDateTime(ExifIfd0Directory.TagDateTime, out var datetime))
+                photo.AddMetadata("DateTime", datetime);
+        }
+
+        public static void ParseSubIfd(this ExifSubIfdDirectory directory, Photo photo)
+        {
+            if (directory.TryGetSingle(ExifDirectoryBase.TagFNumber, out var fNum))
+                photo.AddMetadata("F-Number", fNum);
+
+            if (directory.TryGetInt16(ExifDirectoryBase.TagIsoEquivalent, out var iso))
+                photo.AddMetadata("ISO", iso);
+            
+            if (directory.TryGetSingle(ExifDirectoryBase.TagShutterSpeed, out var apexValue))
+                photo.AddMetadata("ShutterSpeed", MetadataConversion.ComputeShutterSpeed(apexValue));
+
+            if (directory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out var dateTimeOriginal))
+                photo.AddMetadata("DateTimeOriginal", dateTimeOriginal);
+
+            if (directory.TryGetSingle(ExifDirectoryBase.TagFocalLength, out var focalLength))
+                photo.AddMetadata("FocalLength", focalLength);
+        }
+
+        public static void Parse(this JpegDirectory directory, Photo photo)
+        {
+            if (directory.TryGetInt32(JpegDirectory.TagImageHeight, out var height))
+                photo.AddMetadata("Height", height);
+
+            if (directory.TryGetInt32(JpegDirectory.TagImageWidth, out var width))
+                photo.AddMetadata("Width", width);
+        }
+
+        public static void Parse(this PngDirectory directory, Photo photo)
+        {
+            if (directory.TryGetInt32(PngDirectory.TagImageHeight, out var height))
+                photo.AddMetadata("Height", height);
+
+            if (directory.TryGetInt32(PngDirectory.TagImageWidth, out var width))
+                photo.AddMetadata("Width", width);
+        }
+    }
+}
---
+++ PhotoOrganizerLib/Utils/MetadataConverter.cs
@@ -0,0 +1,47 @@
+using System;
+using MetadataExtractor;
+
+namespace PhotoOrganizerLib.Utils
+{
+    public static class MetadataConverter
+    {
+        /// <summary>Private method for converting Degree/Minutes/Seconds to decimal degrees.</summary>
+        /// <returns>Double of the coordinate.</returns>
+        /// <remarks>If input array is not of size 3, return 0.0.</remarks>
+        /// <param name="degMinSec">Rational array containing the Degree/Minutes/Seconds.</param>
+        /// <param name="gpsRef">GPS reference specifying direction, e.g. "N" or "E".</param>
+        public static double DegreesMinutesSecondsToDecimal(Rational[] degMinSec, string gpsRef)
+        {
+            if (degMinSec?.Length != 3 || gpsRef is null) 
+                return 0;
+
+            var hours = Math.Abs(degMinSec[0].ToDouble());
+            double minutes = degMinSec[1].ToDouble();
+            double seconds = degMinSec[2].ToDouble();
+
+            double value = hours + (minutes / 60.0d) + (seconds / 3600.0d);
+
+            // If Ref is not N or E, negate the value.
+            if (gpsRef == "S" || gpsRef == "W")
+                value *= -1;
+            
+            return value;
+        }
+
+        public static string ComputeShutterSpeed(float apexValue)
+        {
+            if (apexValue <= 1)
+            {
+                var apexPower = (float)(1 / Math.Exp(apexValue * Math.Log(2)));
+                var apexPower10 = (long)Math.Round(apexPower * 10.0);
+                var fApexPower = apexPower10 / 10.0f;
+                return fApexPower + " sec";
+            }
+            else
+            {
+                var apexPower = (int)Math.Exp(apexValue * Math.Log(2));
+                return "1/" + apexPower + " sec";
+            }
+        }
+    }
+}
