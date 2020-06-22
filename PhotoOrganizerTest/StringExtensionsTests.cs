c88b567b5d502621df4455534a0defe01d9e63ab
haysch <36488481+haysch@users.noreply.github.com>
2020-06-22 21:53:05 +0200

    Add tests for string extensions

---
+++ PhotoOrganizerTest/StringExtensionsTests.cs
@@ -0,0 +1,70 @@
+using Xunit;
+
+using PhotoOrganizerLib.Extensions;
+using System;
+
+namespace PhotoOrganizerTest
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void IsYear_True()
+        {
+            var yearString = "2000";
+
+            var actual = yearString.IsYear();
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void IsYear_False()
+        {
+            var yearString = "25b";
+
+            var actual = yearString.IsYear();
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IsYear_Null()
+        {
+            string yearString = null;
+            
+            var actual = yearString.IsYear();
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IsMonth_True()
+        {
+            var monthString = "05";
+
+            var actual = monthString.IsMonth();
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void IsMonth_False()
+        {
+            var monthString = "13";
+
+            var actual = monthString.IsMonth();
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IsMonth_Null()
+        {
+            string monthString = null;
+            
+            var actual = monthString.IsMonth();
+
+            Assert.False(actual);
+        }
+    }
+}
