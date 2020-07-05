f89ae5632fc5ee65ad5449ce670826fb23dbf5ee
haysch <36488481+haysch@users.noreply.github.com>
2020-07-05 20:08:49 +0200

    Remove unused method. Update method names.

--- PhotoOrganizerLib/Utils/ParseMetadata.cs
+++ PhotoOrganizerLib/Utils/ParseMetadata.cs
@@ -18,41 +18,13 @@ namespace PhotoOrganizerLib.Utils
         public static void Parse(Photo photo, IEnumerable<Directory> directories)
         {
             var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
-            subIfdDirectory?.ParseSubIfd(photo);
+            subIfdDirectory?.Parse(photo);
 
             var gpsDirectory = directories.OfType<GpsDirectory>().FirstOrDefault();
-            gpsDirectory?.ParseGps(photo);
+            gpsDirectory?.Parse(photo);
 
             var ifd0Directory = directories.OfType<ExifIfd0Directory>().FirstOrDefault();
-            ifd0Directory?.ParseIFD0(photo);
-        }
-
-        /// <summary>Parse <see cref="MetadataExtractor.Directory" /> metadata and save the information in the <see cref="PhotoOrganizerLib.Models.Photo" /> object.</summary>
-        /// <param name="directory">MetadataExtractor directory object containing the image metadata.</param>
-        /// <param name="photo">Photo object to save the metadata to.</param>
-        private static void ParseDirectory(Directory directory, Photo photo)
-        {
-            // TODO: There might be an easier way
-            switch (directory)
-            {
-                case ExifSubIfdDirectory subIfd:
-                    subIfd?.ParseSubIfd(photo);
-                    break;
-                case GpsDirectory gps:
-                    gps?.ParseGps(photo);
-                    break;
-                case ExifIfd0Directory ifd0:
-                    ifd0?.ParseIFD0(photo);
-                    break;
-                case JpegDirectory jpeg:
-                    jpeg?.ParseJpeg(photo);
-                    break;
-                case PngDirectory png:
-                    png?.ParsePng(photo);
-                    break;
-                default:
-                    break;
-            }
+            ifd0Directory?.Parse(photo);
         }
     }
 }
