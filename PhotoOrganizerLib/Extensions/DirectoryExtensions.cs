957b6d8712f49ce815533a773820dc58e2588c08
haysch <36488481+haysch@users.noreply.github.com>
2020-07-05 20:04:28 +0200

    Create default Parse method for unhandled directories. Rename all methods to Parse for generalization.

--- PhotoOrganizerLib/Extensions/DirectoryExtensions.cs
+++ PhotoOrganizerLib/Extensions/DirectoryExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.Jpeg;
@@ -9,10 +10,19 @@ namespace PhotoOrganizerLib.Extensions
 {
     public static class DirectoryExtensions
     {
+        /// <summary>Default parse method for handling unsupported directories.</summary>
+        /// <param name="directory">Directory containing the some metadata.</param>
+        /// <param name="photo">Photo object used for storing metadata.</param>
+        /// <remarks>This method is meant as a handler where the directory parsing has not been implemented yet.</remarks>
+        public static void Parse(this Directory directory, Photo photo)
+        {
+            Console.WriteLine($"Directory type { directory.GetType() } is not supported for parsing.");
+        }
+
         /// <summary>Parses <see cref="MetadataExtractor.Formats.Exif.GpsDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
         /// <param name="directory">Directory containing the GPS metadata.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
-        public static void ParseGps(this GpsDirectory directory, Photo photo)
+        public static void Parse(this GpsDirectory directory, Photo photo)
         {
             if (directory is null || photo is null)
             {
@@ -28,12 +38,14 @@ namespace PhotoOrganizerLib.Extensions
             var latitude = MetadataConverter.DegreesMinutesSecondsToDecimalDegrees(gpsLat, gpsLatRef);
             var longitude = MetadataConverter.DegreesMinutesSecondsToDecimalDegrees(gpsLon, gpsLonRef);
 
+            // if we do not have both latitude and longitude, do not save coordinates
             if (!(latitude is null) && !(longitude is null))
             {
                 photo.AddMetadata("Latitude", latitude);
                 photo.AddMetadata("Longitude", longitude);
             }
             
+            // if we cannot get both the altitude and its reference, do not save anything
             if (directory.TryGetByte(GpsDirectory.TagAltitudeRef, out var gpsAltBit) &&
                 directory.TryGetInt16(GpsDirectory.TagAltitude, out var gpsAlt))
             {
@@ -46,7 +58,7 @@ namespace PhotoOrganizerLib.Extensions
         /// <summary>Parses <see cref="MetadataExtractor.Formats.Exif.ExifIfd0Directory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
         /// <param name="directory">Directory containing the make, model and datetime metadata.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
-        public static void ParseIFD0(this ExifIfd0Directory directory, Photo photo)
+        public static void Parse(this ExifIfd0Directory directory, Photo photo)
         {
             if (directory is null || photo is null)
             {
@@ -69,7 +81,7 @@ namespace PhotoOrganizerLib.Extensions
         /// <summary>Parses <see cref="MetadataExtractor.Formats.Exif.ExifSubIfdDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
         /// <param name="directory">Directory containing the F-number, ISO, shutter speed, datetime original and focal length metadata.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
-        public static void ParseSubIfd(this ExifSubIfdDirectory directory, Photo photo)
+        public static void Parse(this ExifSubIfdDirectory directory, Photo photo)
         {
             if (directory is null || photo is null)
             {
@@ -95,7 +107,7 @@ namespace PhotoOrganizerLib.Extensions
         /// <summary>Parses <see cref="MetadataExtractor.Formats.Jpeg.JpegDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
         /// <param name="directory">Directory containing the JPEG image height and width.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
-        public static void ParseJpeg(this JpegDirectory directory, Photo photo)
+        public static void Parse(this JpegDirectory directory, Photo photo)
         {
             if (directory is null || photo is null)
             {
@@ -112,7 +124,7 @@ namespace PhotoOrganizerLib.Extensions
         /// <summary>Parses <see cref="MetadataExtractor.Formats.Png.PngDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
         /// <param name="directory">Directory containing the PNG image height and width.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
-        public static void ParsePng(this PngDirectory directory, Photo photo)
+        public static void Parse(this PngDirectory directory, Photo photo)
         {
             if (directory is null || photo is null)
             {
