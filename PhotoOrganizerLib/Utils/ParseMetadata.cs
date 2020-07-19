272b139b0f2b4213deb66e5aa1295187febb9754
haysch <36488481+haysch@users.noreply.github.com>
2020-07-19 11:56:17 +0200

    Simplify namespace in <see>

--- PhotoOrganizerLib/Utils/ParseMetadata.cs
+++ PhotoOrganizerLib/Utils/ParseMetadata.cs
@@ -9,14 +9,21 @@ using PhotoOrganizerLib.Extensions;
 
 namespace PhotoOrganizerLib.Utils
 {
-    /// <summary>Class for parsing <see cref="MetadataExtractor.Directory" /> to <see cref="PhotoOrganizerLib.Models.Photo" /> metadata dictionary.</summary>
+    /// <summary>Class for parsing <see cref="Directory" /> to <see cref="Photo" /> metadata dictionary.</summary>
     public static class ParseMetadata
     {
-        /// <summary>Parses the list of <see cref="MetadataExtractor.Directory" /> and saves the metadata to the <see cref="PhotoOrganizerLib.Models.Photo" /> object.</summary>
+        /// <summary>Parses the list of <see cref="Directory" /> and saves the metadata to the <see cref="Photo" /> object.</summary>
         /// <param name="photo">Picture object used for extracting metadata and saving the values.</param>
-        /// <param name="directories">Enumerable of <see cref="MetadataExtractor.Directory" />, containing the different image <see cref="MetadataExtractor.Tag" />s.</param>
+        /// <param name="directories">Enumerable of <see cref="Directory" />, containing the different image <see cref="Tag" />s.</param>
         public static void Parse(Photo photo, IEnumerable<Directory> directories)
         {
+            if (directories is null)
+            {
+                // Nothing to do
+                return;
+            }
+            
+            // TODO: Maybe change to returning a Dictionary<string, object> instead of adding to photo?
             directories.OfType<ExifSubIfdDirectory>()
                 .FirstOrDefault()?
                 .Parse(photo);
