1f26c777db247296b25619eacd6cd47da7f4d356
haysch <36488481+haysch@users.noreply.github.com>
2020-07-20 19:58:45 +0200

    Change from dictionary to property-based values

--- PhotoOrganizerLib/Extensions/DirectoryExtensions.cs
+++ PhotoOrganizerLib/Extensions/DirectoryExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
 using MetadataExtractor.Formats.Jpeg;
@@ -10,7 +9,7 @@ namespace PhotoOrganizerLib.Extensions
 {
     public static class DirectoryExtensions
     {
-        /// <summary>Parses <see cref="MetadataExtractor.Formats.Exif.GpsDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
+        /// <summary>Parses <see cref="GpsDirectory" /> metadata and saves it to the <see cref="Photo" />.</summary>
         /// <param name="directory">Directory containing the GPS metadata.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
         public static void Parse(this GpsDirectory directory, Photo photo)
@@ -29,24 +28,19 @@ namespace PhotoOrganizerLib.Extensions
             var latitude = MetadataConverter.DegreesMinutesSecondsToDecimalDegrees(gpsLat, gpsLatRef);
             var longitude = MetadataConverter.DegreesMinutesSecondsToDecimalDegrees(gpsLon, gpsLonRef);
 
-            // if we do not have both latitude and longitude, do not save coordinates
-            if (!(latitude is null) && !(longitude is null))
-            {
-                photo.TryAddMetadata("Latitude", latitude);
-                photo.TryAddMetadata("Longitude", longitude);
-            }
+            photo.Latitude = latitude;
+            photo.Longitude = longitude;
             
             // if we cannot get both the altitude and its reference, do not save anything
             if (directory.TryGetByte(GpsDirectory.TagAltitudeRef, out var gpsAltBit) &&
                 directory.TryGetInt16(GpsDirectory.TagAltitude, out var gpsAlt))
             {
-                var gpsAltRef = gpsAltBit == 0 ? "Sea level" : "Below sea level";
-                photo.TryAddMetadata("AltitudeRef", gpsAltRef);
-                photo.TryAddMetadata("Altitude", gpsAlt);
+                photo.AltitudeReference = gpsAltBit == 0 ? "Sea level" : "Below sea level";
+                photo.Altitude = gpsAlt;
             }
         }
 
-        /// <summary>Parses <see cref="MetadataExtractor.Formats.Exif.ExifIfd0Directory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
+        /// <summary>Parses <see cref="ExifIfd0Directory" /> metadata and saves it to the <see cref="Photo" />.</summary>
         /// <param name="directory">Directory containing the make, model and datetime metadata.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
         public static void Parse(this ExifIfd0Directory directory, Photo photo)
@@ -59,17 +53,14 @@ namespace PhotoOrganizerLib.Extensions
             var make = directory.GetString(ExifIfd0Directory.TagMake);
             var model = directory.GetString(ExifIfd0Directory.TagModel);
 
-            if (!(make is null))
-                photo.TryAddMetadata("Make", make);
-
-            if (!(model is null))
-                photo.TryAddMetadata("Model", model);
+            photo.Make = make;
+            photo.Model = model;
 
             if (directory.TryGetDateTime(ExifIfd0Directory.TagDateTime, out var datetime))
-                photo.TryAddMetadata("DateTime", datetime);
+                photo.DateTime = datetime;
         }
 
-        /// <summary>Parses <see cref="MetadataExtractor.Formats.Exif.ExifSubIfdDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
+        /// <summary>Parses <see cref="ExifSubIfdDirectory" /> metadata and saves it to the <see cref="Photo" />.</summary>
         /// <param name="directory">Directory containing the F-number, ISO, shutter speed, datetime original and focal length metadata.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
         public static void Parse(this ExifSubIfdDirectory directory, Photo photo)
@@ -80,22 +71,22 @@ namespace PhotoOrganizerLib.Extensions
             }
 
             if (directory.TryGetSingle(ExifDirectoryBase.TagFNumber, out var fNum))
-                photo.TryAddMetadata("F-Number", fNum);
+                photo.FNumber = fNum;
 
             if (directory.TryGetInt16(ExifDirectoryBase.TagIsoEquivalent, out var iso))
-                photo.TryAddMetadata("ISO", iso);
-            
+                photo.Iso = iso;
+
             if (directory.TryGetSingle(ExifDirectoryBase.TagShutterSpeed, out var apexValue))
-                photo.TryAddMetadata("ShutterSpeed", MetadataConverter.ComputeShutterSpeed(apexValue));
+                photo.ShutterSpeed = MetadataConverter.ComputeShutterSpeed(apexValue);
 
             if (directory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out var dateTimeOriginal))
-                photo.TryAddMetadata("DateTimeOriginal", dateTimeOriginal);
+                photo.DateTimeOriginal = dateTimeOriginal;
 
             if (directory.TryGetSingle(ExifDirectoryBase.TagFocalLength, out var focalLength))
-                photo.TryAddMetadata("FocalLength", focalLength);
+                photo.FocalLength = focalLength;
         }
 
-        /// <summary>Parses <see cref="MetadataExtractor.Formats.Jpeg.JpegDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
+        /// <summary>Parses <see cref="JpegDirectory" /> metadata and saves it to the <see cref="Photo" />.</summary>
         /// <param name="directory">Directory containing the JPEG image height and width.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
         public static void Parse(this JpegDirectory directory, Photo photo)
@@ -104,15 +95,15 @@ namespace PhotoOrganizerLib.Extensions
             {
                 return;
             }
-            
+
             if (directory.TryGetInt32(JpegDirectory.TagImageHeight, out var height))
-                photo.TryAddMetadata("Height", height);
+                photo.Height = height;
 
             if (directory.TryGetInt32(JpegDirectory.TagImageWidth, out var width))
-                photo.TryAddMetadata("Width", width);
+                photo.Width = width;
         }
 
-        /// <summary>Parses <see cref="MetadataExtractor.Formats.Png.PngDirectory" /> metadata and saves it to the <see cref="PhotoOrganizerLib.Models.Photo" />.</summary>
+        /// <summary>Parses <see cref="PngDirectory" /> metadata and saves it to the <see cref="Photo" />.</summary>
         /// <param name="directory">Directory containing the PNG image height and width.</param>
         /// <param name="photo">Photo object used for storing metadata.</param>
         public static void Parse(this PngDirectory directory, Photo photo)
@@ -123,10 +114,10 @@ namespace PhotoOrganizerLib.Extensions
             }
 
             if (directory.TryGetInt32(PngDirectory.TagImageHeight, out var height))
-                photo.TryAddMetadata("Height", height);
+                photo.Height = height;
 
             if (directory.TryGetInt32(PngDirectory.TagImageWidth, out var width))
-                photo.TryAddMetadata("Width", width);
+                photo.Width = width;
         }
     }
 }
