1402693faf219454eea43b391a517efbd6f4872c
haysch <36488481+haysch@users.noreply.github.com>
2020-07-21 21:39:17 +0200

    Implement organizer flow

--- PhotoOrganizerLib/Services/OrganizerService.cs
+++ PhotoOrganizerLib/Services/OrganizerService.cs
@@ -3,10 +3,11 @@ using Microsoft.Extensions.Configuration;
 using PhotoOrganizerLib.Utils;
 using PhotoOrganizerLib.Extensions;
 using System.IO;
-using System.Collections.Generic;
 using MetadataExtractor;
 using PhotoOrganizerLib.Data;
 using System.Threading.Tasks;
+using PhotoOrganizerLib.Enums;
+using System;
 
 namespace PhotoOrganizerLib.Services
 {
@@ -28,22 +29,32 @@ namespace PhotoOrganizerLib.Services
             // if input directory does not exist, throw exception and end run
             inputDirectory.EnsureDirectoryExists();
 
+            var hashAlgorithm = _configuration.GetValue<Algorithm>("hash-algorithm");
+            var checksum = new Checksum(hashAlgorithm);
+
             await foreach (var photo in PhotoHandler.FindPhotosAsync(inputDirectory))
             {
-                var metadataDirectories = FetchMetadataDirectories(photo.FilePath);
+                using var fs = File.OpenRead(photo.FilePath);
+
+                // Compute checksum
+                photo.Checksum = checksum.ComputeChecksum(fs);
+
+                // Reset filestream position
+                fs.Position = 0;
+
+                // Fetch metadata directories using MetadataExctractor and parse metadata to the Photo object
+                var metadataDirectories = ImageMetadataReader.ReadMetadata(fs);
                 ParseMetadata.Parse(photo, metadataDirectories);
+
+                // Rename and sort photos
                 _sortService.SortPhoto(photo);
 
-                await _context.AddAsync(photo);
+                // Add photo to database context
+                await _context.Photos.AddAsync(photo);
             }
 
+            // Save all additions to the database
             await _context.SaveChangesAsync();
-        }
-
-        private static IEnumerable<MetadataExtractor.Directory> FetchMetadataDirectories(string path)
-        {
-            using var fs = File.OpenRead(path);
-            return ImageMetadataReader.ReadMetadata(fs);
         }
     }
 }
