808895a992e26e78d75ad1e9d5fe1309b36bca16
haysch <36488481+haysch@users.noreply.github.com>
2020-07-21 21:37:37 +0200

    Mark DbSet as null-forgiving

--- PhotoOrganizerLib/Data/PhotoContext.cs
+++ PhotoOrganizerLib/Data/PhotoContext.cs
@@ -7,6 +7,6 @@ namespace PhotoOrganizerLib.Data
     {
         public PhotoContext(DbContextOptions<PhotoContext> options) : base(options) { }
 
-        public DbSet<Photo>? Photos { get; set; }
+        public DbSet<Photo> Photos { get; set; } = null!;
     }
 }
