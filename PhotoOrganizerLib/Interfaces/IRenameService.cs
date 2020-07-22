e25be33ce513d03715cb16f1d96551076c92d8db
haysch <36488481+haysch@users.noreply.github.com>
2020-07-22 22:16:59 +0200

    Rename argument

--- PhotoOrganizerLib/Interfaces/IRenameService.cs
+++ PhotoOrganizerLib/Interfaces/IRenameService.cs
@@ -5,6 +5,6 @@ namespace PhotoOrganizerLib.Interfaces
     public interface IRenameService
     {
         string FindPhotoDateTime(Photo photo, string format);
-        void RenameFile(string sourcePath, string targetPath);
+        void RenameFile(string sourcePath, string destPath);
     }
 }
