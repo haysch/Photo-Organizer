e0903460ae82b22cccf5060aa8d2a1c02b64d605
haysch <36488481+haysch@users.noreply.github.com>
2020-07-22 21:05:02 +0200

    Use correct nullable reference for DbSet

--- PhotoOrganizerLib/Data/PhotoContext.cs
+++ PhotoOrganizerLib/Data/PhotoContext.cs
@@ -7,6 +7,6 @@ namespace PhotoOrganizerLib.Data
     {
         public PhotoContext(DbContextOptions<PhotoContext> options) : base(options) { }
 
-        public DbSet<Photo> Photos { get; set; } = null!;
+        public DbSet<Photo> Photos => Set<Photo>();
     }
 }
