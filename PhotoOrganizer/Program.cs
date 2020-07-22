3185825c4a63e6be136053c6ca99414ade74002a
haysch <36488481+haysch@users.noreply.github.com>
2020-07-22 22:40:25 +0200

    Add main flow for organizing image files! Beta

--- PhotoOrganizer/Program.cs
+++ PhotoOrganizer/Program.cs
@@ -1,21 +1,97 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PhotoOrganizer.Enums;
+using PhotoOrganizerLib.Data;
+using PhotoOrganizerLib.Interfaces;
+using PhotoOrganizerLib.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace PhotoOrganizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var configuration = BuildConfiguration(args);
+            var serviceProvider = ConfigureServices(configuration);
 
+            using var scope = serviceProvider.CreateScope();
+            var services = scope.ServiceProvider;
+            var organizerService = services.GetRequiredService<IOrganizerService>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            var inputPath = configuration.GetValue<string>("input");
+            if (inputPath is null)
+            {
+                logger.LogError($"No input path is specified. Use [--input|-i] /path/to/input/dir parameter. Exiting.");
+                return;
+            }
+            else
+            {
+                await organizerService.RunOrganizerAsync(inputPath);
+            }
         }
 
-        private static IConfiguration BuildConfig(string[] args)
+        static IConfiguration BuildConfiguration(string[] args)
         {
+            var switchMappings = new Dictionary<string, string>
+            {
+                { "-i", "input" },
+                { "-o", "output" },
+                { "-db", "database" },
+                { "-rt", "rename-type" },
+                { "-ha", "hash-algorithm" }
+            };
+
             return new ConfigurationBuilder()
-                // .AddJsonFile($"{System.IO.Directory.GetCurrentDirectory()}/appsettings.json", false) // TODO is there any point in using a settings file?
-                .AddEnvironmentVariables("PHORG_")
-                .AddCommandLine(args)
+                // .AddEnvironmentVariables(PREFIX) // Needs more refinement
+                .AddCommandLine(args, switchMappings)
                 .Build();
+        }
+
+        static IServiceProvider ConfigureServices(IConfiguration configuration)
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(logger => 
+            {
+                logger.ClearProviders();
+                logger.AddConsole();
+            });
+
+            // Configure database
+            var databaseFlag = configuration.GetValue<DatabaseFlag>("database");
+            services.AddDbContext<PhotoContext>(options =>
+            {
+                switch (databaseFlag)
+                {
+                    case DatabaseFlag.MySQL:
+                        options.UseMySQL("");
+                        break;
+                    case DatabaseFlag.PostgreSQL:
+                        options.UseNpgsql("");
+                        break;
+                    case DatabaseFlag.SQLite:
+                    default:
+                        var outputPath = configuration.GetValue<string>("output") ?? Directory.GetCurrentDirectory();
+                        var dbPath = Path.Combine(outputPath, "PhotoOrganizer.db");
+                        options.UseSqlite($"Data Source={dbPath}");
+                        break;
+                }
+            });
+
+            // Setup DI for services
+            services.AddSingleton<IConfiguration>(configuration)
+                .AddSingleton<IRenameService, RenameService>()
+                .AddSingleton<ISortService, SortService>()
+                .AddSingleton<IOrganizerService, OrganizerService>();
+
+            return services.BuildServiceProvider();
         }
     }
 }
